de2742cd9debc808d36318a36af9d937b376f98d
Zineb Berrachedi <berrachedizineb5@gmail.com>
2025-07-10 15:53:19 +0100

    add main route and fix form submission

--- fontend.py
+++ fontend.py
@@ -1,10 +1,16 @@
 from flask import Flask , render_template, request
 app=Flask(__name__)
+@app.route('/',methods=['GET'])
+def home():
+  return render_template('form.html')
+
+
 @app.route('/Thnks',methods=['GET', 'POST'])
-def index():
+def thank_you():
     if request.method == 'POST':
-        return render_template('form.html')
-    else:
+        print("ok")
         return render_template('thankyou.html')
+    else:
+        return render_template('form.html')
 if __name__=="__main__":
     app.run(debug=True)
