baeab706c0fbb79c6c4229e586c0d5d3f9a70afe
Zineb Berrachedi <berrachedizineb5@gmail.com>
2025-07-10 14:39:52 +0100

    Changed File location + fixed some bugs

--- fontend.py
+++ fontend.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 @app.route('/Thnks',methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return render_template('thankyou.html')
+        return render_template('form.html')
     else:
         return render_template('thankyou.html')
 if __name__=="__main__":
