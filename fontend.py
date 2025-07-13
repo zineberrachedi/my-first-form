6a1958adcd29e6bc86ed68b6647fc75a136bf38f
Zineb Berrachedi <berrachedizineb5@gmail.com>
2025-07-13 14:46:24 +0100

    fix to preserve user input on errors

--- fontend.py
+++ fontend.py
@@ -16,21 +16,27 @@ def thank_you():
       email=request.form.get("email")
       tel=request.form.get("tel")
       gender=request.form.get("gender")
+      text=request.form.get("text1")
+      country=request.form.get("country")
       if not fname:
        error_message = "First name is required :)"
-       return render_template('form.html',error_message=error_message)
+       return render_template('form.html',error_message=error_message,fname=fname,lname=lname,gender=gender,email=email,tel=tel,text=text,country=country)
       
       if not lname:
        error_message = "Last name is required :)"
-       return render_template('form.html',error_message=error_message)
+       return render_template('form.html',error_message=error_message,fname=fname,lname=lname,gender=gender,email=email,tel=tel,text=text,country=country)
       
       if not email:
        error_message = "Email is required :)"
-       return render_template('form.html',error_message=error_message)
+       return render_template('form.html',error_message=error_message,fname=fname,lname=lname,gender=gender,email=email,tel=tel,text=text,country=country)
       
       if not gender:
        error_message = "choose your gender please :)"
-       return render_template('form.html',error_message=error_message)
+       return render_template('form.html',error_message=error_message,fname=fname,lname=lname,gender=gender,email=email,tel=tel,text=text,country=country)
+      if not tel:
+        error_message = "tel is required :)"
+        return render_template('form.html',error_message=error_message,fname=fname,lname=lname,gender=gender,email=email,tel=tel,text=text,country=country)
+
 
       return render_template('thankyou.html')
    else:
